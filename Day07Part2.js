f4b23190351fe87cc873382d6dac0a4b36cc1d05
Jacob Varns <jacob.varns@gmail.com>
2021-12-06 21:22:09 -0800

    Create Day07Part2.js

---
+++ Day07Part2.js
@@ -0,0 +1,39 @@
+let vals = document.querySelector('pre').innerText.split(',');
+let parsedVals = [];
+let highest = 0;
+let lowest = 9999;
+vals.forEach(val=>{
+    let temp = parseInt(val);
+    parsedVals.push(temp);
+    if(temp > highest) {
+        highest = temp;
+    }
+    if(temp < lowest) {
+        lowest = temp;
+    }
+})
+
+let lowestFuelCost = 999999999;
+let lowIdx = -1;
+
+function getFuelCost(dist){
+    let temp = 0;
+    for(let i=1;i<=dist;i++) {
+        temp+=i;
+    }
+    return temp;
+}
+
+for(let i = 0; i<=highest; i++) {
+    let fuelCost = 0;
+    for(let j = 0; j<parsedVals.length; j++) {
+        fuelCost += getFuelCost(Math.abs(parsedVals[j]-i));
+    }
+    if(fuelCost < lowestFuelCost) {
+        lowestFuelCost = fuelCost;
+        lowIdx = i;
+    }
+}
+
+console.log(lowestFuelCost);
+console.log(lowIdx);
